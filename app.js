9b47a1352d7dfbc9289c66731a33e851e41fe06f
Luisa Novaes <luisanovaes.arq@gmail.com>
2022-04-28 21:15:45 -0300

    Uploaded files
    Add style to the project.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const input = document.querySelector(".input")
-const btn = document.querySelector(".btn")
+const btn = document.querySelector(".short-btn")
 const txt = document.querySelector(".url-txt")
 
 //API url 
@@ -16,10 +16,17 @@ input.addEventListener("change", (event)=>{
 
 btn.addEventListener("click", (e)=>{
     e.preventDefault()
+    if (inputValue === "") {
+        txt.innerHTML = "Please add a link"
+        txt.style.color ="#f46262"
+        txt.style.fontStyle = "italic"
+    } 
     fetch(`${base}shorten?url=${inputValue}`)                   //Solicitar dados da API e exibir esses dados em um local do html
         .then(response => response.json())
         .then(data =>{
             txt.innerHTML = data.result.short_link
+            txt.style.color ="#2acfcf"
+            txt.style.fontStyle = "normal"
     })
 })
 
